73f05e418d941fe253a37717ee258081061a8d33
Yuji Hanamura <yuji.developer@gmail.com>
2022-12-07 19:34:49 +0900

    Support Rails 6.0 and 6.1

--- lib/best_in_place/engine.rb
+++ lib/best_in_place/engine.rb
@@ -1,8 +1,11 @@
 module BestInPlace
-  class Engine < Rails::Engine
+  class Engine < ::Rails::Engine
     initializer 'best_in_place' do
-      ActionView::Base.send(:include, BestInPlace::Helper)
-      ActionController::Base.send(:include, BestInPlace::ControllerExtensions)
+      ::ActiveSupport.on_load(:action_controller) do
+        ::ActionView::Base.send(:include, ::BestInPlace::Helper)
+
+        ::ActionController::Base.send(:include, ::BestInPlace::ControllerExtensions)
+      end
     end
   end
 end
